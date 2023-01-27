f691ea70f6f229d2e4f9b95baca17e5ac7b08e75
Chris Boettner <boettnec@gmail.com>
2023-01-27 11:01:10 +0100

    added type hints

--- src/customdecorator.py
+++ src/customdecorator.py
@@ -7,9 +7,11 @@ Created on Thu Jan 26 16:59:26 2023
 """
 
 import functools
+from typing import Callable
 import time
 
-def timing(decimals=2):
+
+def timing(decimals: int = 2):
     '''Timing function usable as decorator'''
     # outer function to accept argument for number of decimal points for timer
     def decorator(func):
@@ -20,14 +22,15 @@ def timing(decimals=2):
             start_time = time.perf_counter()
             value = func(*args, **kwargs)
             end_time = time.perf_counter()
-            run_time  = round(end_time-start_time, decimals)
+            run_time = round(end_time - start_time, decimals)
             print(f'Function {func.__name__!r} took {run_time} seconds.')
             return value
-        return wrap 
+        return wrap
     return decorator
 
-def debug(func):
-    ''''Print the function signature and return value.'''
+
+def debug(func: Callable):
+    '''Print the function signature and return value.'''
     @functools.wraps(func)
     def wrapper_debug(*args, **kwargs):
         args_repr = [repr(a) for a in args]
@@ -38,3 +41,16 @@ def debug(func):
         print(f"{func.__name__!r} returned {value!r}.")
         return value
     return wrapper_debug
+
+
+class CountCalls:
+    '''Add and increment 'calls' attribute to function.'''
+
+    def __init__(self, func: Callable):
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.calls = 0
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        return self.func(*args, **kwargs)
